14d1cdddba9f0a498f98023afe3cda4139fc0dc1
MiyalalMulla <mullamiyalal@gmail.com>
2025-09-29 14:26:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // your code here
+let nameInput = document.getElementById("name");
+let yearInput = document.getElementById("year");
+let button = document.getElementById("button");
+let urlElement = document.getElementById("url");
+
+button.addEventListener("click",(e)=>{
+	e.preventDefault();
+	let baseUrl = "https://localhost:8080/";
+	let para = [];
+	if(nameInput.value.trim()){
+		para.push("name=" + encodeURIComponent(nameInput.value.trim()));
+	}
+	if(yearInput.value.trim()){
+		para.push("year" + encodeURIComponent(yearInput.value.trim()));
+	}
+	if (para.length > 0) {
+		baseUrl += "?" + para.join("&");
+	}
+	urlElement.textContent = baseUrl;
+});
+ 
+
